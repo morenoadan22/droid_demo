317e7e92fdb5193e1fef68029136cc35a8dea21a
Adan Moreno <adanm@fb.com>
2022-02-04 09:01:07 -0800

    move layout to onCreate

--- app/src/main/java/com/training/demo/MainActivity.kt
+++ app/src/main/java/com/training/demo/MainActivity.kt
@@ -7,24 +7,20 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
-    private lateinit var frameContainer : ViewGroup
+    private lateinit var container : ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        frameContainer = findViewById(R.id.container)
-    }
-
-    override fun onStart() {
-        super.onStart()
+        container = findViewById(R.id.container)
 
         val helloWorldText = TextView(this).apply {
             text = getString(R.string.hello_text)
             setTextColor(Color.BLACK)
         }
 
-        frameContainer.addView(helloWorldText, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        container.addView(helloWorldText, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
     }
 }
