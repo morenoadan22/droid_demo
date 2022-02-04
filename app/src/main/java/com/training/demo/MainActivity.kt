4fcc3fae222292948581e2622d099650603ba478
Adan Moreno <adanm@fb.com>
2022-02-04 10:15:27 -0800

    add demo button

--- app/src/main/java/com/training/demo/MainActivity.kt
+++ app/src/main/java/com/training/demo/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.training.demo
 
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
@@ -21,6 +24,16 @@ class MainActivity: AppCompatActivity() {
             setTextColor(Color.BLACK)
         }
 
+        val clickMeButton = Button(this).apply {
+            text = getString(R.string.click_me)
+            setTextColor(Color.BLACK)
+            setBackgroundColor(Color.LTGRAY)
+            setOnClickListener {
+                Toast.makeText(this@MainActivity, "Button Clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         container.addView(helloWorldText, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        container.addView(clickMeButton)
     }
 }
